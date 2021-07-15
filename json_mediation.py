efc47256f5f06226d341ed57bbb24827ef1974b9
wbligh <billybligh95@gmail.com>
2021-07-15 15:12:07 +0100

    set up mediation to trawl link collection for new recipe url, then scraper to pass JSON to recipe collection

--- json_mediation.py
+++ json_mediation.py
@@ -15,29 +15,36 @@ from pymongo import MongoClient
 
 
 client = MongoClient("mongodb+srv://dbUser_WB:A8KWLm-Ad_fPC-J@cluster0.xb4rm.mongodb.net/db_grub_recipe?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE", connect = False)
-db = client.test
 
 
 
 
 
+'''
 
-url = "https://www.bbcgoodfood.com/recipes/oregano-chicken-squash-traybake"
 url_2 = 'https://www.bbcgoodfood.com/recipes/sweet-potatoes-red-pepper-halloumi'
 url_3 = 'https://www.bbcgoodfood.com/recipes/pepper-steak-with-noodles'
 url_4 = 'https://www.bbcgoodfood.com/recipes/pasta-alla-norma'
 url_5 = 'https://www.bbcgoodfood.com/recipes/pork-fennel-burgers-fennel-slaw'
 
+'''
+
+db = client.get_database('db_grub_recipe')
+recipe_records = db.cl_GF_recipes
+recipe_link_records = db.cl_GF_recipe_links
+
+
+scraped_links = [d['url'] for d in list(recipe_records.find({},{'_id':False,'url':True}))]
+url = recipe_link_records.find_one({'url':{'$nin':scraped_links}},{'_id':False, 'recipe_url':True})['recipe_url']
+
 # Retrieve URL and initialise recipe object
 recipe = sc.compile_recipe(url)
 
+recipe_records.insert(recipe)
 
-db = client.get_database('db_grub_recipe')
 
-records = db.cl_GF_recipes
 
 #   records.insert_one(recipe)
 
-result = records.find_one({'url': recipe['url']})
 
 
